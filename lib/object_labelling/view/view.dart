035ef582e692a89423c3aec200ff0abc781b0728
Hadiya Aamir <hadiyaaamir222@gmail.com>
2025-07-01 00:05:11 +0500

    cleanup

--- lib/object_labelling/view/view.dart
+++ lib/object_labelling/view/view.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:camera/camera.dart';
 import 'package:ml_flutter/common/common.dart';
-import 'package:ml_flutter/object_labelling/object_labelling.dart';
-import 'package:ml_flutter/services/services.dart';
 import 'package:ml_flutter/ml_media/ml_media.dart';
+import 'package:ml_flutter/object_labelling/object_labelling.dart';
 
 part 'object_labelling_page.dart';
 part 'object_labelling_screen.dart';
